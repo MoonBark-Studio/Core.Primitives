49bd4b4a53645d87b7e5d058b9fda7a53dd3c91b
Chris' Tutorials <christutorialsyt@gmail.com>
2026-03-08 14:11:06 -0400

    Add ECS core components

---
+++ src/Core/ECS/IECSSystem.cs
@@ -0,0 +1,21 @@
+﻿namespace MoonBark.Core.ECS;
+
+using Friflo.Engine.ECS;
+
+/// <summary>
+/// Interface for ECS systems that process entities in the world.
+/// </summary>
+public interface IECSSystem
+{
+    /// <summary>
+    /// Priority for system execution order (lower = earlier).
+    /// </summary>
+    int Priority { get; }
+
+    /// <summary>
+    /// Updates the system with the given world and delta time.
+    /// </summary>
+    /// <param name="world">The ECS world containing entities to process.</param>
+    /// <param name="deltaTime">Time elapsed since the last update in seconds.</param>
+    void Update(EntityStore world, float deltaTime);
+}
